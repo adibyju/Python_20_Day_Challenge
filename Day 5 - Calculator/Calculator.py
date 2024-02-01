d77e2504d8feca478c185d6d8af8c6fb5b3db25d
adibyju <adityabyju7@gmail.com>
2024-02-01 11:52:25 +0530

    Python code for Calculator app

--- Day 5 - Calculator/Calculator.py
+++ Day 5 - Calculator/Calculator.py
@@ -6,23 +6,24 @@ import pyshorteners
 
 
 class CalculatorApp(QWidget):
+    """Main application class for a basic calculator using PyQt5."""
+
     def __init__(self):
         QWidget.__init__(self)
+
+        # Setup window properties
         self.setWindowTitle("Calculator")
         self.setGeometry(100, 100, 400, 400)
 
+        # Define display areas for calculation and result
         self.CalculationSpace = QtWidgets.QLineEdit(self)
         self.CalculationSpace.setReadOnly(True)
         self.CalculationSpace.setMaxLength(50)
 
         self.ResultSpace = QtWidgets.QLabel(self)
-        self.ResultSpace.setAlignment(
-            QtCore.Qt.AlignCenter | QtCore.Qt.AlignVCenter
-        )  # for center align
+        self.ResultSpace.setAlignment(QtCore.Qt.AlignCenter | QtCore.Qt.AlignVCenter)
         self.ResultSpace.setObjectName("result_space")
-        self.ResultSpace.setStyleSheet(
-            "QLabel#result_space {color: blue}"
-        )  # change the colour of the result
+        self.ResultSpace.setStyleSheet("QLabel#result_space {color: blue}")
 
         self.ResultLabel = QtWidgets.QLabel(self)
         self.ResultLabel.setText("RESULT:")
@@ -32,12 +33,12 @@ class CalculatorApp(QWidget):
         layoutResult.addWidget(self.ResultLabel)
         layoutResult.addWidget(self.ResultSpace)
 
-        # Delete horizontal layout
+        # Delete button horizontal layout
         self.DeleteButton = QtWidgets.QPushButton("Del")
         layoutDelete = QtWidgets.QHBoxLayout()
         layoutDelete.addItem(
             QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
-        )  # add a horizontal spacer item to push the button to the right
+        )
         layoutDelete.addWidget(self.DeleteButton)
 
         # 1st row of calculator
@@ -95,7 +96,7 @@ class CalculatorApp(QWidget):
         layout5thRow.addWidget(self.DotButton)
         layout5thRow.addWidget(self.EqualButton)
 
-        # create a vertical layout of all the components using QVBoxLayout
+        # Create a vertical layout of all the components using QVBoxLayout
         layoutMain = QtWidgets.QVBoxLayout()
         layoutMain.addWidget(self.CalculationSpace)
         layoutMain.addLayout(layoutResult)
@@ -106,16 +107,17 @@ class CalculatorApp(QWidget):
         layoutMain.addLayout(layout4thRow)
         layoutMain.addLayout(layout5thRow)
 
-        # set the layout to the main app
+        # Set the layout to the main app
         self.setLayout(layoutMain)
 
+        # Initialize the count of open parentheses
         self.numOpenParenthesis = 0
 
+        # Connect buttons to their respective functions
         self.ResetButton.clicked.connect(self.reset_function)
         self.DeleteButton.clicked.connect(self.delete_function)
         self.EqualButton.clicked.connect(self.eval_function)
 
-        # bind functions to the buttons
         self._0Button.clicked.connect(lambda: self.add_function(self._0Button))
         self._1Button.clicked.connect(lambda: self.add_function(self._1Button))
         self._2Button.clicked.connect(lambda: self.add_function(self._2Button))
@@ -145,10 +147,12 @@ class CalculatorApp(QWidget):
         self.SignButton.clicked.connect(lambda: self.add_function(self.SignButton))
 
     def reset_function(self):
+        """Clears all input and resets the calculator."""
         self.numOpenParenthesis = 0
         self.CalculationSpace.setText("")
 
     def delete_function(self):
+        """Deletes the last character from the input field."""
         temp = self.CalculationSpace.text()
         if temp == "":
             return
@@ -159,6 +163,7 @@ class CalculatorApp(QWidget):
         self.CalculationSpace.setText(temp[:-1])
 
     def eval_function(self):
+        """Evaluates the expression entered in the calculator."""
         exp = self.CalculationSpace.text()
         exp = exp.replace("%", "/100")
         for char in exp:
@@ -178,9 +183,11 @@ class CalculatorApp(QWidget):
         self.ResultSpace.setText(str(result))
 
     def find_function(self, x):
+        """Finds the index of the first non-digit character in the given string."""
         return next((i for i, char in enumerate(x) if not char.isdigit()), -1)
 
     def add_function(self, button):
+        """Adds the character of the pressed button to the calculation space."""
         calcText = self.CalculationSpace.text()
         bText = button.text()
         result = ""
@@ -252,6 +259,7 @@ class CalculatorApp(QWidget):
         self.CalculationSpace.setText(result)
 
 
+# Main entry point for the application
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     mainApp = CalculatorApp()
